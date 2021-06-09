26002bfa7fbfb9e7565e3494e9a2873e3370bae6
MoyshaGG <56951957+MoyshaGG@users.noreply.github.com>
2021-06-10 01:56:10 +0300

    Add files via upload

---
+++ Exam-master/Exam/Dobriy.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam
+{
+    public class Dobriy : Student
+    {
+        private Random _random;
+
+        public Dobriy(string name, string surname, string middleName, int attendedClasses) : base(name, surname, middleName, attendedClasses)
+        {
+            _random = new Random();
+        }
+
+        public override bool PassExam()
+        {
+            if (AttendedClasses == CountOfLessons)
+                return true;
+            if (AttendedClasses > CountOfLessons / 2)
+            {
+                if (_random.NextDouble() < 0.7)
+                    return false;
+                else
+                    return true;
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Surname: {Surname}, Middle name: {MiddleName}, Attended classes: {AttendedClasses}";
+        }
+
+    }
+}
---
+++ Exam-master/Exam/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exam
+{
+    public partial class Form1 : Form
+    {
+        private readonly Student[] _students;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _students = new Student[]
+            {
+
+                new UsualStudent("Добрий", "Stoida", "Oleksandrovich", 40),
+                new UsualStudent("Добрий", "Stoida1", "Oleksandrovich1", 34),
+                new UsualStudent("Добрий", "Stoida2", "Oleksandrovich2", 36),
+                new UsualStudent("Добрий", "Stoida3", "Oleksandrovich3", 26),
+                new UsualStudent("Добрий", "Stoida4", "Oleksandrovich4", 31),
+
+                new CleverS("Привіт", "Денисов", "Абобов", 35),
+                new CleverS("Привіт", "Хумус", "Бобас", 25),
+                new CleverS("Привіт", "Сівір", "Сіріус", 24),
+                new CleverS("Привіт", "Бібас", "Бобас", 23),
+                new GeniusStudent("Валерій", "Жмишенко", "Альбертович", 1),
+
+                new Dobriy("Добрий день", "Zavyalov", "Serggich", 35),
+                new Dobriy("Добрий день", "Oler", "Norde", 40),
+                new Dobriy("Добрий день", "Plata", "Ohus", 20),
+                new Dobriy("Добрий день", "Punk", "Gibis", 27),
+                new Dobriy("Добрий день", "Abo", "Vivo", 27),
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var student in _students)
+            {
+                if (student.PassExam())
+                    textBox1.Text += student + ", Student passed exam" + Environment.NewLine;
+                else
+                    textBox1.Text += student + ", Student doesn't pass exam" + Environment.NewLine;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
